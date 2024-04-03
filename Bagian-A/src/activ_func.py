fix: softmax calculations

Bagian-A/src/activ_func.py
@@ -14,21 +14,31 @@ class Activation_Function(Enum):
     SOFTMAX = "softmax"
 
 
-def relu(v: float):
-    return max(0, v)
+def reluElmt(v: float):
+    return max(0.0, v)
 
+def sigmoidElmt(v: float):
+    return 1.0 / (1.0 + math.exp(-v))
 
-def sigmoid(v: float):
-    return 1 / (1 + math.exp(-v))
-
-def softmaxVect(v: float):
-    e_v = np.exp(v - np.max(v)) 
-    return e_v / e_v.sum(axis=0)
+def softmaxElmt(v: float, expSum: float):
+    e_v = np.exp(v) 
+    return e_v / expSum
 
 
 """
 Vectorizing activation functions so
 functions can be easily applied to ndarrays
 """
-reluVect = np.vectorize(relu)
-sigmoidVect = np.vectorize(sigmoid)
+reluVect = np.vectorize(reluElmt)
+sigmoidVect = np.vectorize(sigmoidElmt)
+softmaxVect = np.vectorize(softmaxElmt)
+
+def softmax(mat: np.ndarray):
+    mat = mat.transpose()
+    new_mat = np.empty((0,mat.shape[1]))
+    for row_idx in range(0, mat.shape[0]):
+        row = mat[row_idx]
+        row_exp_sum = np.sum(np.exp(row))
+        sm = softmaxVect(row, row_exp_sum)
+        new_mat = np.append(new_mat, np.array([sm]), axis=0)
+    return new_mat.transpose()

Bagian-A/src/ffnn.py
@@ -1,7 +1,7 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import json
-from activ_func import Activation_Function, reluVect, sigmoidVect, softmaxVect
+from activ_func import Activation_Function, reluVect, sigmoidVect, softmax
 
 """
 Layer object class
@@ -53,7 +53,7 @@ class FFNN:
         
         o_flat = output.ravel()
         e_flat = self._expected.ravel()
-        sse = 0
+        sse = 0.0
         for idx, o_val in enumerate(o_flat):
             e_val = e_flat[idx]
             sse += (o_val - e_val) ** 2
@@ -73,18 +73,18 @@ class FFNN:
         current = np.array(self._input).transpose()
         bias = np.array([[1.0 for _ in self._input]])
 
-        for layer in self._layers:
+        for idx, layer in enumerate(self._layers):
             current = np.concatenate((bias, current), axis=0)
 
             new_current = layer.w.transpose() @ current
             current = new_current
-            
+
             if layer.activ_func == Activation_Function.RELU:
                 current = reluVect(current)
             elif layer.activ_func == Activation_Function.SIGMOID:
                 current = sigmoidVect(current)
             elif layer.activ_func == Activation_Function.SOFTMAX:
-                current = softmaxVect(current)
+                current = softmax(current)
 
         current = current.transpose()
         sse = self.calc_sse(current)